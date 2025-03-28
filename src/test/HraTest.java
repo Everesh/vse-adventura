f4b2b36c472258f81bd70d53abb9db8ef774abeb
Jan Jurka <jurj16@vse.cz>
2025-03-28 20:03:52 +0100

    Add first two steps

--- src/test/HraTest.java
+++ src/test/HraTest.java
@@ -16,14 +16,23 @@ public class HraTest {
     @Test
     public void zakladniTestovaciScenar() {
         // Uvitani - zapnuti hry
-        assertEquals("Vítejte!\n" +
-                "Toto je příběh o Červené Karkulce, babičce a vlkovi.\n" +
-                "Napište 'nápověda', pokud si nevíte rady, jak hrát dál.\n" +
-                "\n" +
-                "Jsi v mistnosti/prostoru domeček, ve kterém bydlí Karkulka.\nvýchody: les", hra.vratUvitani());
-        // 1. krok hry JDI les
-        assertEquals( "Jsi v mistnosti/prostoru les s jahodami, malinami a pramenem vody.\n" +
-                "východy: domeček hluboký_les" ,hra.zpracujPrikaz("jdi les"));
+        assertEquals(hra.vratUvitani(),
+                "TODO\n" +
+                       "Jsi ve své cele\n" +
+                       "Máš na sobě: okovy\n" +
+                       "Seznam věcí v batohu:\n" +
+                       "Seznam věcí v místnosti: tělo_žalářníka, zámek_okovů\n" +
+                       "Východy: severní_koridor_žaláře");
+
+        // 1. krok hry PROZKOUMEJ tělo_žalářníka
+        assertEquals(hra.zpracujPrikaz("prozkoumej tělo_žalářníka"),
+                "Prozkoumáváš tělo_žalářníka\n" +
+                       "Máš na sobě: okovy\n" +
+                       "Seznam věcí v batohu:\n" +
+                       "Poznatky:\n" +
+                       "Obsahuje: žalářníkův_úbor, klíč_od_cel\n" +
+                       "Východy: moje_cela");
+
         // 2. krok hry JDI hluboky_les
         assertEquals( "Jsi v mistnosti/prostoru temný les, ve kterém lze potkat vlka.\n" +
                 "východy: chaloupka jeskyně les",hra.zpracujPrikaz("jdi hluboký_les"));
