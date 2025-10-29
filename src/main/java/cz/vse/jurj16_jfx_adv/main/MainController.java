ddf2fcc0502199cd239e1df435c425c5654a01cc
Jan Jurka <jurj16@vse.cz>
2025-10-29 02:39:47 +0100

    chore: vid 7

--- src/main/java/cz/vse/jurj16_jfx_adv/main/MainController.java
+++ src/main/java/cz/vse/jurj16_jfx_adv/main/MainController.java
@@ -1,5 +1,8 @@
 package cz.vse.jurj16_jfx_adv.main;
 
+import cz.vse.jurj16_jfx_adv.logika.Hra;
+import cz.vse.jurj16_jfx_adv.logika.IHra;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -12,8 +15,24 @@ public class MainController {
     @FXML
     private TextField vstup;
 
+    private IHra hra = new Hra();
+
+    @FXML
+    private void initialize() {
+        vystup.appendText(hra.vratUvitani() + "\n\n");
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vstup.requestFocus();
+            }
+        });
+    }
+
     public void odesliVstup(ActionEvent actionEvent) {
-        vystup.appendText(vstup.getText() + '\n');
+        String prikaz = vystup.getText();
+        vystup.appendText("> " + vstup.getText() + '\n');
+        String vysledek = hra.zpracujPrikaz(prikaz);
+        vystup.appendText(vysledek + "\n\n");
         vstup.clear();
     }
 }
